4739c6eb5b0bb305195db12ebff9a0aac37692b0
schang1146 <sammychang2185@gmail.com>
2020-02-04 19:44:34 -0800

    implemented brute force for stock_prices.py

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,15 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  # Brute force method:
+  profit_max = prices[1] - prices[0]
+
+  for price_low_idx in range(0, len(prices)):
+    for price_high_idx in range(price_low_idx + 1, len(prices)):
+      if (prices[price_high_idx] - prices[price_low_idx]) > profit_max:
+        profit_max = prices[price_high_idx] - prices[price_low_idx]
+
+  return profit_max
 
 
 if __name__ == '__main__':
