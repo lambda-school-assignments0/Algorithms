2678dc5e8bdaf086b87703a35e7bccab2b2937f7
schang1146 <sammychang2185@gmail.com>
2020-02-05 00:34:31 -0800

    almost got rps.py; nesting too much

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -2,9 +2,21 @@
 
 import sys
 
-def rock_paper_scissors(n):
-  pass 
+def rock_paper_scissors_helper(arr, n):
+  if len(arr) != n:
+    return \
+    rock_paper_scissors_helper(arr + ['rock'], n), \
+    rock_paper_scissors_helper(arr + ['paper'], n), \
+    rock_paper_scissors_helper(arr + ['scissors'], n)
+  else:
+    return arr
 
+def rock_paper_scissors(n):
+  if n == 0:
+    return [[]]
+  else:
+    return \
+      [*rock_paper_scissors_helper([], n)]
 
 if __name__ == "__main__":
   if len(sys.argv) > 1:
