da6b9a0eee83d17ac0b2771f5630396eb963753e
schang1146 <sammychang2185@gmail.com>
2020-02-05 19:52:06 -0800

    finished making_change.py

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,7 +3,24 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
+  ### Recursive solution
+  # ¯\_(ツ)_/¯
+  
+  ### Dynamic programming w/ matrix
+  # Initialize 'bank'
+  bank = [[0 for _ in range(amount + 1)] for _ in range(len(denominations) + 1)]
+  # Initialize base case
+  bank[0][0] = 1
+  bank[1][:] = [1 for _ in range(amount + 1)]
+  # Calculate number of ways to make change
+  for row in range(2,len(bank)):
+    for col in range(len(bank[0])):
+      if col >= denominations[row-1]:
+        bank[row][col] = bank[row][col-denominations[row-1]] + bank[row-1][col]
+      else:
+        bank[row][col] = bank[row-1][col]
+
+  return bank[-1][-1]
 
 
 if __name__ == "__main__":
